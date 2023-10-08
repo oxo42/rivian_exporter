01f3ae3e6e5573fbbff717928419bd0f040f84a9
John Oxley <john.oxley@gmail.com>
2023-10-07 20:30:46 -0700

    small change

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -10,7 +10,7 @@ from .pytest_testslide import testslide
 
 def test_set_prom_metrics():
     """
-    This actually sets the prometheus metrics.  If I were a Good Engineer I'd go
+    This actually sets the prometheus metrics.  If I were a Good Engineer™️ I'd go
     verify that they were actually called correctly.  The main thing I want here
     is to ensure I didn't fat-finger any of the keys so am calling it quits.
     P.S. I love PRs
